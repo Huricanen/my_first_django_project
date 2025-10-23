f07902cc182ae0048faf396d3be63c7570d45188
Huricanen <rikex@yandex.ru>
2025-10-23 19:03:57 +0300

    Сделано куча всякого

---
+++ app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Product, Brand, Category
+
+def index(request):
+    products = Product.objects.all()
+    brands = Brand.objects.all()
+    categories = Category.objects.all()
+
+    context = {
+        "products": products,
+        "brands": brands,
+        "categories": categories
+    }
+    return render(request, "app/index.html", context=context)
